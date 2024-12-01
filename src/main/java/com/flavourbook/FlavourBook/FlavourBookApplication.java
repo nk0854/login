2abafd4beba0295683bd7a0e4e338727abfbb8c2
Nikhil <nikhilkr8804@gmail.com>
2024-12-01 20:12:12 +0530

    Updated

---
+++ src/main/java/com/flavourbook/FlavourBook/FlavourBookApplication.java
@@ -0,0 +1,13 @@
+package com.flavourbook.FlavourBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavourBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavourBookApplication.class, args);
+	}
+
+}
